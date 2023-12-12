70f98f51b41a43517b60ac31b16f5dfd58590c9d
Landon Thull <landonthull.tx@gmail.com>
2023-12-12 10:59:55 -0800

    working posts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
 
-
-export default authMiddleware();
+export default authMiddleware({
+  publicRoutes: ["/api/trpc/posts.getAll"],
+});
 
 export const config = {
-    matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
+  matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,29 @@
 import { SignInButton, useUser } from "@clerk/nextjs";
 import Head from "next/head";
 
-import { RouterOutputs, api } from "~/utils/api";
+import { type RouterOutputs, api } from "~/utils/api";
 
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
 import { LoadingPage } from "~/components/loading";
+import { useState } from "react";
 
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
 
-  console.log(user);
+  const [input, setInput] = useState("");
+
+  const ctx = api.useUtils();
+
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void ctx.posts.getAll.invalidate();
+    },
+  });
 
   if (!user) return null;
 
@@ -29,7 +39,12 @@ const CreatePostWizard = () => {
       <input
         placeholder="Type some emojis!"
         className="grow bg-transparent outline-none"
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        disabled={isPosting}
       />
+      <button onClick={() => mutate({ content: input })}>Submit</button>
     </div>
   );
 };
@@ -69,7 +84,7 @@ const Feed = () => {
 
   return (
     <div className="flex flex-col">
-      {[...data, ...data]?.map((fullPost) => (
+      {data?.map((fullPost) => (
         <PostView {...fullPost} key={fullPost.post.id} />
       ))}
     </div>
